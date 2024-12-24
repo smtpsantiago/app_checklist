54da720eb86cb9f9bd13eb0045bdc630dac17a01
Jose becerra <jose.becerra@softtek.com>
2024-12-24 02:33:48 +0000

    prototipo 1

--- appchecklist/webapp/controller/View1.controller.js
+++ appchecklist/webapp/controller/View1.controller.js
@@ -6,41 +6,69 @@ sap.ui.define([
 
     return Controller.extend("appchecklist.appchecklist.controller.View1", {
         onInit() {
-            const oData = {
-                DatosNumericos: [
-                  {
-                    Actividad: "Mediciones eléctricas del motor",
-                    Tarea: "Medición resistencia entre bobinas",
-                    Bornes: "U - V",
-                    Medicion: "10 Ω",
-                    MedicionAnterior: "12 Ω"
-                  },
-                  {
-                    Actividad: "Mediciones eléctricas del motor",
-                    Tarea: "Medición resistencia entre bobinas",
-                    Bornes: "U - W",
-                    Medicion: "11 Ω",
-                    MedicionAnterior: "11 Ω"
-                  },
-                  {
-                    Actividad: "Mediciones eléctricas del motor",
-                    Tarea: "Medición resistencia entre bobinas",
-                    Bornes: "V - W",
-                    Medicion: "9 Ω",
-                    MedicionAnterior: "8 Ω"
-                  },
-                  {
-                    Actividad: "Mediciones eléctricas del motor",
-                    Tarea: "Medición resistencia entre bobinas",
-                    Bornes: "U - TIERRA",
-                    Medicion: "0 Ω",
-                    MedicionAnterior: "0 Ω"
-                  }
-                ]
-              };
-
-              const oModel = new JSONModel(oData);
-              this.getView().setModel(oModel);
+          const oData = {
+            DatosNumericos: [
+              {
+                Actividad: "Mediciones eléctricas del motor",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "U - V",
+                Medicion: "10 Ω",
+                MedicionAnterior: "12 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas del motor",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "U - W",
+                Medicion: "11 Ω",
+                MedicionAnterior: "11 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas del motor",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "V - W",
+                Medicion: "9 Ω",
+                MedicionAnterior: "8 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas del motor",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "U - TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "0 Ω"
+              }
+            ],
+            CheckList: [
+              {
+                Actividad: "Calificación Diseño",
+                Tarea: "Vida útil del instrumento/obsolescencia",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Diseño",
+                Tarea: "Resistencia a golpes y vibraciones hasta 4G",
+                Estado: "No Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Diseño",
+                Tarea: "Equipo cumple con clasificación UL seguridad intrínseca",
+                Estado: "No Aplica",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Diseño",
+                Tarea: "Equipo cumple para trabajo en atmósfera explosiva ATEX",
+                Estado: "Aprueba",
+                Observaciones: ""
+              }
+            ]
+          };
+    
+          // Crear modelo JSON único
+          const oModel = new JSONModel(oData);
+          // Asignar el modelo combinado a la vista
+          this.getView().setModel(oModel);
         }
     });
 });
