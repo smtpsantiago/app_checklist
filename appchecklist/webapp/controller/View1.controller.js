4c0e0e76f53a655f91da028be8728e91f87ae52d
Jose becerra <jose.becerra@softtek.com>
2025-01-07 13:45:14 +0000

    version 2

--- appchecklist/webapp/controller/View1.controller.js
+++ appchecklist/webapp/controller/View1.controller.js
@@ -35,6 +35,76 @@ sap.ui.define([
                 Bornes: "U - TIERRA",
                 Medicion: "0 Ω",
                 MedicionAnterior: "0 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas del motor",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "V - TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "6 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas del motor",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "W - TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "0 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas en alimentadores",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "L1-L2",
+                Medicion: "0 Ω",
+                MedicionAnterior: "10 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas en alimentadores",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "L1-L3",
+                Medicion: "0 Ω",
+                MedicionAnterior: "10 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas en alimentadores",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "L2-L3",
+                Medicion: "0 Ω",
+                MedicionAnterior: "50 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas en alimentadores",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "L1-TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "10 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas en alimentadores",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "L2-TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "10 Ω"
+              },
+              {
+                Actividad: "Mediciones eléctricas en alimentadores",
+                Tarea: "Medición resistencia entre bobinas",
+                Bornes: "L3-TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "10 Ω"
+              },
+              {
+                Actividad: "Mediciones puesta a tierra",
+                Tarea: "Valor maximo 5 Ω",
+                Bornes: "TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "4 Ω"
+              },
+              {
+                Actividad: "Mediciones puesta a tierra",
+                Tarea: "Valor maximo 5 Ω",
+                Bornes: "TIERRA",
+                Medicion: "0 Ω",
+                MedicionAnterior: "4 Ω"
               }
             ],
             CheckList: [
@@ -61,7 +131,157 @@ sap.ui.define([
                 Tarea: "Equipo cumple para trabajo en atmósfera explosiva ATEX",
                 Estado: "Aprueba",
                 Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Correcta ubicación y orientación del equipo",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Montaje del instrumento con espacio libre para evitar darios mecanicos",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Acceso y visibili dad del instrumento",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Revision de sujeción de conectores gland",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Instrumento y caja de conexiones se encuentra hermeticamente sellado",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Inspección y reposición de amarras cables",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Deterioro fisico del equipo (sulfatación, corrosión)",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Estado de conexiones a tierra (limpieza, cortadas, no instaladas)",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Revisión de elementos mecánicos de sujeción tornillos, pernos (instalados)",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Instalación",
+                Tarea: "Instrumento esta correctamente identificado en campo 'tag' (rotulado)",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Limpieza e inspeccion visual del equipo",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Reconexion de todos los terminales y verificacion de apriete de los mismos",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Comprobacion tension de alimentacion del equipo 24 vdc",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Revisar el filtro de la linea base y en caso de que no lo tenga, reponer.(manual del fabricante) ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Revisar parametros de configuracion del equipo (unidad de ingenieria etcetera) ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Revisar deterioro fisico interno en caja de conexiones del detector (sulfatacion, corrosion, componentes)",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Operación",
+                Tarea: "Engrase de tornilleria exterior y sellado ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Comprobar indicador LED (verde) del detector en estado pasivo ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Ajuste del punto Zero atmosferico",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Ajuste del SPAN con gas patron certificado por el fabricante (Sensibilidad) ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Comprobar estado de luces LED del detector durante los ajustes de sensibilidad y zero atmosferico ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Detector funciona consistentemente y entrega reproducibilidad en sistema de control ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Corroborar normalizacion del estado de alarma en sistema de control ",
+                Estado: "Aprueba",
+                Observaciones: ""
+              },
+              {
+                Actividad: "Calificación Desempeño",
+                Tarea: "Requiere calibracion",
+                Estado: "Aprueba",
+                Observaciones: ""
               }
+            ],
+            Estado: [
+              { Select: "Aprueba" },
+              { Select: "No Aprueba" },
+              { Select: "No Aplica" }
+
             ]
           };
     
